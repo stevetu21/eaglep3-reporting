a4f5998fd03ebe825d3fa01cb36b587b1cbae8be
Steve Tu <stu@xorail.com>
2017-03-27 12:15:39 -0600

    latter march

---
+++ Troubleshooting MindMap - ST.mm
@@ -0,0 +1,6 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1490213218563" ID="ID_235207509" MODIFIED="1490213253791" TEXT="Trip with red &quot;Y&quot; in Concerning column">
+<node CREATED="1490213318329" ID="ID_248010921" MODIFIED="1490213337718" POSITION="right" TEXT="Kibana trace looks abnormal?"/>
+</node>
+</map>
